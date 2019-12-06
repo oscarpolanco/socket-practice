0d21d6bbba2eaad968f6136d345c5be4fb379b85
Oscar Polanco <opolanco@intellisys.com.do>
2019-12-06 19:29:52 -0400

    Emit the count event to all connections

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -21,7 +21,7 @@ io.on('connection', (socket) => {
 
     socket.on('increment', () => {
         count++;
-        socket.emit('countUpdated', count);
+        io.emit('countUpdated', count);
     });
 });
 
