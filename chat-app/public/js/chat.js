9a34de8b7fbc21e74757b28e40ca621203cb80a6
Oscar Polanco <opolanco@intellisys.com.do>
2019-12-07 15:09:07 -0400

    Separate the location message from the normal message event

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -18,6 +18,10 @@ socket.on("message", message => {
   $messages.insertAdjacentHTML('beforeend', html);
 });
 
+socket.on("locationMessage", (url) => {
+  console.log(url);
+});
+
 $messageForm.addEventListener("submit", e => {
   e.preventDefault();
 
