eeb8b17b8d9ed4d21ec552a83c57cb2402070757
Oscar Polanco <itt.oscarpolanco@gmail.com>
2019-12-06 22:21:46 -0500

    Send and recive messages between clients

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -16,6 +16,10 @@ io.on("connection", socket => {
   console.log("New WebSocket connection");
 
   socket.emit("message", "Welcome");
+
+  socket.on("sendMessage", message => {
+    io.emit("message", message);
+  });
 });
 
 server.listen(port, () => {
