3eb4b170a439232476f4e928bfe1395d27c283bd
Oscar Polanco <opolanco@intellisys.com.do>
2019-12-09 15:58:09 -0400

    Add username when you send a message and the admin messages

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -27,8 +27,8 @@ io.on("connection", socket => {
 
     socket.join(user.room);
 
-    socket.emit("message", generateMessage("Welcome!"));
-    socket.broadcast.to(user.room).emit("message", generateMessage(`${user.username} has joined!`));
+    socket.emit("message", generateMessage("Admin", "Welcome!"));
+    socket.broadcast.to(user.room).emit("message", generateMessage("Admin", `${user.username} has joined!`));
 
     callback();
   });
@@ -41,7 +41,7 @@ io.on("connection", socket => {
       return callback("Profanity is not allow");
     }
 
-    io.to(user.room).emit("message", generateMessage(message));
+    io.to(user.room).emit("message", generateMessage(user.username, message));
     callback("Delivered!");
   });
 
@@ -50,7 +50,7 @@ io.on("connection", socket => {
 
     io.to(user.room).emit(
       "locationMessage",
-      generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+      generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
     );
     callback();
   });
@@ -59,7 +59,7 @@ io.on("connection", socket => {
     const user = removeUser(socket.id);
 
     if (user) {
-        io.to(user.room).emit("message", generateMessage(`${user.username} has left!`));
+        io.to(user.room).emit("message", generateMessage("Admin", `${user.username} has left!`));
     }
   });
 });
