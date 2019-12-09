d12d0b393af10ee4120bcc21885c52019da3afc3
Oscar Polanco <opolanco@intellisys.com.do>
2019-12-09 16:42:58 -0400

    Create a sidebar template an populate with data

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -10,6 +10,7 @@ const $messages = document.querySelector('#messages');
 // Template
 const messageTemplate = document.querySelector("#message-template").innerHTML;
 const locationMessageTemplate = document.querySelector("#location-message-template").innerHTML;
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
 
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true});
@@ -23,6 +24,14 @@ socket.on("message", message => {
     createdAt: moment(message.createdAt).format("h:mm a")
   });
   $messages.insertAdjacentHTML('beforeend', html);
+});
+
+socket.on("roomData", ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users
+  });
+  document.querySelector("#sidebar").innerHTML = html;
 });
 
 socket.on("locationMessage", (message) => {
