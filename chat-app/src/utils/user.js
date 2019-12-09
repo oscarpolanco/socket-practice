f63fc093d1a5a022379f839eb67e43b42de89654
Oscar Polanco <opolanco@intellisys.com.do>
2019-12-08 22:50:43 -0400

    Add a function to store the users on a specific room

---
+++ chat-app/src/utils/user.js
@@ -0,0 +1,52 @@
+const users = [];
+
+const addUser = ({ id, username, room}) => {
+    // Clean the data
+    username = username.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are require'
+        };
+    }
+
+    // Check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username;
+    });
+
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
+        };
+    }
+
+    // Store user
+    const user = {
+        id,
+        username,
+        room
+    };
+    users.push(user);
+
+    return { user };
+}
+
+addUser({
+    id: 22,
+    username: 'Andrew   ',
+    room: 'South Philly'
+});
+
+console.log(users);
+
+const res = addUser({
+    id: 33,
+    username: 'Andrew',
+    room: 'South Philly'
+});
+
+console.log(res);
